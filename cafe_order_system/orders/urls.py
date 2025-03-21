f550a9bcf7a82542ddb52864dfa0377979fa6eb7
andrejartemovic <a.mironov95@mail.ru>
2025-03-21 16:30:45 +0300

    format code

--- cafe_order_system/orders/urls.py
+++ cafe_order_system/orders/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import OrderCreate, OrderList, OrderDetail, OrderUpdate, OrderDelete
+
 app_name = "orders"
 
 urlpatterns = [
@@ -7,6 +8,5 @@ urlpatterns = [
     path("orders/list/", OrderList.as_view(), name="order_list"),
     path("orders/<int:pk>", OrderDetail.as_view(), name="order_detail"),
     path("orders/<int:pk>/update", OrderUpdate.as_view(), name="order_update"),
-    path("orders/<int:pk>/delete", OrderDelete.as_view(), name="order_delete")
-
+    path("orders/<int:pk>/delete", OrderDelete.as_view(), name="order_delete"),
 ]
